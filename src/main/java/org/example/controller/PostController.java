91d0dfcae064a83a3b95d45b3451973d120152b6
nesterov <dnestterov@gmail.com>
2023-09-13 08:45:27 +0600

    Change di-java

--- src/main/java/org/example/controller/PostController.java
+++ src/main/java/org/example/controller/PostController.java
@@ -4,34 +4,28 @@ import com.google.gson.Gson;
 import org.example.exception.NotFoundException;
 import org.example.model.Post;
 import org.example.service.PostService;
-import org.springframework.stereotype.Controller;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
 
-@Controller
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
-
     public PostController(PostService service) {
         this.service = service;
     }
-
     public void getAllPosts(HttpServletResponse response) throws IOException {
         response.setContentType(APPLICATION_JSON);
         final var data = service.all();
         final var gson = new Gson();
         response.getWriter().print(gson.toJson(data));
     }
-
     public void getById(long id, HttpServletResponse response) throws IOException {
         final var gson = new Gson();
         final var post = service.getById(id);
         response.getWriter().print(gson.toJson(post));
     }
-
     public void save(Reader body, HttpServletResponse response) throws IOException {
         try {
             response.setContentType(APPLICATION_JSON);
@@ -45,7 +39,6 @@ public class PostController {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
     }
-
     public void removeById(long id, HttpServletResponse response) throws IOException {
         final var gson = new Gson();
         response.getWriter().print(gson.toJson(service.getById(id)) + "\n Post was deleted!");
